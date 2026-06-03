300eef369bb34b4a00b0b6d9dd378ae3f43c98be
Chinonso Xavier <101524269+chinonsoxavier@users.noreply.github.com>
2026-06-03 07:43:28 +0100

    Update project-data.ts

--- lib/project-data.ts
+++ lib/project-data.ts
@@ -141,29 +141,11 @@ export const allProjects = [
       "Dashboard Development",
       "API Integration",
     ],
-    link: "http://semailer.app/",
+    link: "http://seamailer.app/",
     image: [semailer],
     featured: true,
   },
-  {
-    name: "Pdf Plug",
-    category: "SaaS", // Changed from "Productivity SaaS"
-    role: "Frontend Developer",
-    duration: "6 Weeks",
-    description:
-      "Built an online document management and PDF utility platform that allows users to process, manage, and interact with PDF documents through a streamlined web interface.",
-    tools: [
-      "React",
-      "JavaScript",
-      "Tailwind CSS",
-      "API Integration",
-      "Responsive Design",
-    ],
-    link: "https://pdfplug.com",
-    image: [pdfplug],
-  },
-
-  {
+{
     name: "Noja360",
     category: "SaaS", // Changed from "Business Management SaaS"
     role: "Full Stack Developer",
@@ -186,6 +168,25 @@ export const allProjects = [
     featured: true,
   },
 
+  {
+    name: "Pdf Plug",
+    category: "SaaS", // Changed from "Productivity SaaS"
+    role: "Frontend Developer",
+    duration: "6 Weeks",
+    description:
+      "Built an online document management and PDF utility platform that allows users to process, manage, and interact with PDF documents through a streamlined web interface.",
+    tools: [
+      "React",
+      "JavaScript",
+      "Tailwind CSS",
+      "API Integration",
+      "Responsive Design",
+    ],
+    link: "https://pdfplug.com",
+    image: [pdfplug],
+  },
+
+  
   {
     name: "Evara",
     category: "Business", // Changed from "Business Platform"
